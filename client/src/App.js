0dd0304197ba747a2fa2800558a3485635dc1bb9
seoyeon516 <seoyeon516@gmail.com>
2020-03-27 14:22:22 +0900

    Add Get Customers REST API Module

--- client/src/App.js
+++ client/src/App.js
@@ -18,36 +18,26 @@ const styles = theme => ({
   table:{
     minWidth: 1080
   }
-})
-
-const customers =[
-  {
-  'id':1,
-  'image':'https://placeimg.com/150/150/1',
-  'name':'nam seo yeon',
-  'birthday': '91.05.16',
-  'gender': 'female',
-  'job': 'free-lancer'
-},
-{
-  'id':2,
-  'image':'https://placeimg.com/150/150/2',
-  'name':'park dai sung',
-  'birthday': '74.02.17',
-  'gender': 'male',
-  'job': 'japanese teacher'
-},
-{
-  'id':3,
-  'image':'https://placeimg.com/150/150/3',
-  'name':'kano',
-  'birthday': '03.05.01',
-  'gender': 'male',
-  'job': 'old cat'
-}
-]
+});
 
 class App extends Component{
+
+  state={
+    customers:""
+  }
+
+  componentDidMount(){
+    this.callApi()
+      .then(res => this.setState({customers: res}))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
   render(){
     const { classes } = this.props;
     return(
@@ -65,7 +55,7 @@ class App extends Component{
             </TableRow>
           </TableHead>
           <TableBody>
-          {customers.map(c => {
+          {this.state.customers ? this.state.customers.map(c => {
           return(
             <Customer
             key={c.id}
@@ -77,7 +67,7 @@ class App extends Component{
             job={c.job}
             />
           );
-        })
+        }) : ""
       }
           </TableBody>
         </Table>
--- server.js
+++ server.js
@@ -6,8 +6,33 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
-app.get('/api/hello', (req, res) => {
-    res.send({message: 'Hello Express!'});
+app.get('/api/customers',(req, res) => {
+    res.send([
+        {
+        'id':1,
+        'image':'https://placeimg.com/150/150/1',
+        'name':'nam seo yeon',
+        'birthday': '91.05.16',
+        'gender': 'female',
+        'job': 'free-lancer'
+      },
+      {
+        'id':2,
+        'image':'https://placeimg.com/150/150/2',
+        'name':'park dai sung',
+        'birthday': '74.02.17',
+        'gender': 'male',
+        'job': 'japanese teacher'
+      },
+      {
+        'id':3,
+        'image':'https://placeimg.com/150/150/3',
+        'name':'kano',
+        'birthday': '03.05.01',
+        'gender': 'male',
+        'job': 'old cat'
+      }
+      ]);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
